1cefaf215933767d35511d8f5321e47087a2b6a6
hiravshah6353 <163971546+hiravshah6353@users.noreply.github.com>
2024-03-19 11:04:45 +0530

    my first project
    my first project

---
+++ lab work 1.2.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+main() {
+  printf("1.");
+ 
+    printf("\n my name is hirav shah,\n my age is 20,\n my school name is divyapath");
+    
+    printf("\n2.\n");
+    printf("- - - - - - - -\n");
+
+    printf("|                 |\n");
+    printf("R               |\n");
+    printf("N               |\n");
+    printf("W              |\n");
+    printf("|                 |\n");
+
+    printf("- - - - - - - -\n");
+
+printf("\n3.\n");
+printf("\n*");
+printf("\n**");
+printf("\n***");
+printf("\n**");
+printf("\n*");
+
+printf("\n4.\n");
+printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*                   *   *                        *\n");
+    printf("*               *           *                 *\n");
+    printf("*           *                   *         *\n");
+    printf("*       *                           *  *\n");
+    printf("*    *\n");
+    printf("*");
+}
+ above code output;
+1.
+ my name is hirav shah,
+ my age is 20,
+ my school name is divyapath
+2.
+- - - - - - - -
+|                 |
+R               |
+N               |
+W              |
+|                 |
+- - - - - - - -
+
+3.
+
+*
+**
+***
+**
+*
+4.
+*
+*
+*
+*                   *   *                        *
+*               *           *                 *
+*           *                   *         *
+*       *                           *  *
+*    *
+*
